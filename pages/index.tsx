d8bfa388d8783ec2e1d1df3df0120b4bc1e514c7
Brandon Tsang <brandononline2@gmail.com>
2020-11-15 23:15:18 -0500

    Switched to Next.js.

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+    return (
+        <div>
+            <Head>
+                <title>Slate</title>
+            </Head>
+            <main>
+            </main>
+        </div>
+    );
+}
