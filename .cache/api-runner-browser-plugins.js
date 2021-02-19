e111ce1678197bb224f43b808a80c1d2524a1d91
Brandon Tsang <brandononline2@gmail.com>
2021-02-18 21:02:12 -0500

    Removed unnecessary files.

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-mdx/gatsby-browser.js'),
-      options: {"plugins":[],"extensions":[".mdx","md"],"defaultLayouts":{},"gatsbyRemarkPlugins":[],"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"],"root":"/home/brandon/slate"},
-    }]
--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => (m && m.default) || m
-
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-index-tsx": () => import("./../../../src/pages/index.tsx" /* webpackChunkName: "component---src-pages-index-tsx" */),
-  "component---src-pages-subject-mathematics-tsx": () => import("./../../../src/pages/subject/mathematics.tsx" /* webpackChunkName: "component---src-pages-subject-mathematics-tsx" */)
-}
-
