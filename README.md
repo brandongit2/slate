0bc587de114a557ce3ff09cf1022b7bd58066213
Brandon Tsang <brandononline2@gmail.com>
2021-06-19 18:51:21 -0400

    switch back to next.js lol

--- README.md
+++
@@ -1,3 +0,0 @@
-# Slate&mdash;Learn by doing.
-
-Mathematics, science, and more with interactive demos and virtual labs.
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,73 +1,7 @@
-import {Link} from "gatsby";
-import React from "react";
-import {Helmet} from "react-helmet";
-import styled from "styled-components";
+import {NextPage} from "next"
 
-import "./index.css";
-import SubjectCard from "components/SubjectCard";
-import logo from "images/logotype-dark.svg";
-
-const Container = styled.div`
-  position: absolute;
-  top: 50%;
-  transform: translate(0px, -50%);
-  margin-left: 4rem;
-  display: grid;
-  row-gap: 2rem;
-`;
-
-const Logo = styled.img`
-  height: 2rem;
-`;
-
-const Header = styled.h1`
-  font-size: 32px;
-  font-weight: 800;
-`;
-
-const Subheader = styled.h2`
-  font-size: 18px;
-`;
-
-const Subjects = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  grid-template-rows: auto auto;
-  column-gap: 0.5rem;
-`;
-
-const Subject = styled.div`
-  display: contents;
-`;
-
-export default function Index() {
-  return (
-    <Container>
-      <Helmet>
-        <title>Slate: Learn by doing.</title>
-        <link rel="stylesheet" href="https://use.typekit.net/eqs1cah.css" />
-      </Helmet>
-      <Logo src={logo} />
-      <div>
-        <Header>learn by doing.</Header>
-        <Subheader>
-          mathematics, science, and more with interactive demos and virtual
-          labs.
-        </Subheader>
-      </div>
-      <div>
-        <Subjects>
-          <Link to="/subject/mathematics" key="mathematics">
-            <Subject>
-              <SubjectCard
-                name="mathematics"
-                description="description"
-                color="ff0000"
-              />
-            </Subject>
-          </Link>
-        </Subjects>
-      </div>
-    </Container>
-  );
+const Index: NextPage = () => {
+  return <div>Hello world!</div>
 }
+
+export default Index
