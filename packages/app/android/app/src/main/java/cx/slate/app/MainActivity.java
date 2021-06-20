754d9389d175b55a0e7925ce57d9181e90b888a5
Brandon Tsang <brandononline2@gmail.com>
2021-06-20 02:33:32 -0400

    add android and ios envs

---
+++ packages/app/android/app/src/main/java/cx/slate/app/MainActivity.java
@@ -0,0 +1,5 @@
+package cx.slate.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- packages/app/capacitor.config.ts
+++ packages/app/capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from "@capacitor/cli"
 
 const config: CapacitorConfig = {
-  appId: 'cx.slate.app',
-  appName: 'Slate',
-  webDir: 'public',
-  bundledWebRuntime: false
-};
+  appId: `cx.slate.app`,
+  appName: `Slate`,
+  webDir: `out`,
+  bundledWebRuntime: false,
+}
 
-export default config;
+export default config
