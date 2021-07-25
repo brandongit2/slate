169b07fce831d7360481bfc481760a094b0ad1f5
Brandon Tsang <brandononline2@gmail.com>
2021-07-24 22:30:17 -0400

    remove extra storybook stuff

--- packages/app/.storybook/main.js
+++ packages/app/.storybook/main.js
@@ -3,6 +3,13 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    {name: "@storybook/addon-postcss", options: {postcssLoaderOptions: {implementation: require(`postcss`)}}},
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require(`postcss`),
+        },
+      },
+    },
   ],
 }
