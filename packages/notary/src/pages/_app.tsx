080bc4b0efe0100e097a0fe25764ea6af098780c
Brandon Tsang <brandononline2@gmail.com>
2021-07-28 15:05:45 -0400

    wip

--- packages/notary/src/pages/_app.tsx
+++ packages/notary/src/pages/_app.tsx
@@ -17,10 +17,7 @@ function MyApp({Component, pageProps}: AppProps) {
 
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Gothic+A1:wght@400;700;900&display=swap"
-          rel="stylesheet"
-        />
+        <link href="https://fonts.googleapis.com/css2?family=Alegreya&display=swap" rel="stylesheet" />
       </Head>
       <SlateThemeProvider tw={tw}>
         <RelayEnvironmentProvider environment={environment}>
