8a9bfd6e6737cf4d2a046e00ba36150ee3f9082a
Brandon Tsang <brandononline2@gmail.com>
2021-08-29 23:45:23 -0400

    wip

--- app/src/components/modal/Modal.tsx
+++ app/src/components/modal/Modal.tsx
@@ -1,8 +1,9 @@
 import React, {useContext} from "react"
+import {createPortal} from "react-dom"
 
 import type {FC} from "react"
 
-import ModalContext from "./ModalContext"
+import CloseIcon from "#public/icons/close.svg"
 
 const Modal: FC = ({children}) => {
   const {setIsModalVisible} = useContext(ModalContext)
@@ -11,13 +12,17 @@ const Modal: FC = ({children}) => {
     setIsModalVisible(false)
   }
 
-  return (
+  return createPortal(
     <>
       <div className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50" onClick={handleClick} />
-      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6">
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white p-6">
+        <button className="absolute top-6 right-6" type="button" onClick={handleClick}>
+          <CloseIcon className="h-6" />
+        </button>
         {children}
       </div>
-    </>
+    </>,
+    document.getElementById(`modals`)!,
   )
 }
 
--- app/src/pages/_app.tsx
+++ app/src/pages/_app.tsx
@@ -11,6 +11,7 @@ function MyApp({Component, pageProps}: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
+      <div id="modals"></div>
     </QueryClientProvider>
   )
 }
